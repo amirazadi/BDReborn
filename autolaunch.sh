f37fe972f3a6e6acc0e0e4d89695b85a17c8d870
amirazadi <amazadi79@gmail.com>
2017-03-11 18:05:10 +0330

    autolaunch.sh

---
+++ autolaunch.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+COUNTER=1
+while(true) do
+./beyond.sh
+curl "https://api.telegram.org/bot[358856511:AAHTO_Stsc6C2xmuTSqMm2Cu10NF4_YOxTQ]/sendmessage" -F "chat_id=308444837" -F "text=#NEWCRASH-#TeleBeyond-Reloaded-${COUNTER}-times"
+let COUNTER=COUNTER+1 
+done
