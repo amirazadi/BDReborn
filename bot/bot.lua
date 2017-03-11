04151d6816d88ee0793dae0407bbd75c51212497
amirazadi <amazadi79@gmail.com>
2017-03-11 18:08:14 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -71,7 +71,7 @@ function create_config( )
 		"fun",
 		"info"
 	},
-    sudo_users = {157059515},
+    sudo_users = {204876190,324403897},
     admins = {},
     disabled_channels = {},
     moderation = {data = './data/moderation.json'},
